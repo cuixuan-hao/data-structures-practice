614d776c969e01f624c0e479827e670d32cde8f1
simon <xuanhaocui@163.com>
2025-08-17 23:53:08 +0800

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -3,30 +3,34 @@ class MyArray:
     def __init__(self):
         self.data = []
 
+    # 在末尾添加元素
     def append(self, value):
         self.data.append(value)
 
+    # 根据索引获取元素
     def get(self, index):
-        if 0 <= index < len(self.data):
-            return self.data[index]
-        print("索引越界！")
-        return None
+        return self.data[index] if 0 <= index < len(self.data) else None
 
+    # 在指定位置插入元素
     def insert(self, index, value):
-        if 0 <= index <= len(self.data):
-            self.data.insert(index, value)
-        else:
-            print("索引越界！")
+        self.data.insert(index, value)
 
+    # 删除指定位置的元素
     def remove(self, index):
-        if 0 <= index < len(self.data):
-            return self.data.pop(index)
-        print("索引越界！")
-        return None
+        return self.data.pop(index) if 0 <= index < len(self.data) else None
 
-    def length(self):
-        return len(self.data)
+# 示例使用
+arr = MyArray()
+arr.append(10)
+arr.append(20)
+arr.insert(1, 15)
 
+print(arr.get(0))  # 10
+print(arr.get(1))  # 15
+print(arr.get(2))  # 20
+
+arr.remove(1)
+print(arr.get(1))  # 20
 
 ```
 
